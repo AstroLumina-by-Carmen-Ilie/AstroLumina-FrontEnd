10004eaf44800b8ec7d8d39aefbd629ffe375df5
Pirvu Daniel Catalin <daniel.catalin.pirvu@gmail.com>
2025-03-22 23:11:29 +0200

    Fix rendering on iPhone

--- src/main.tsx
+++ src/main.tsx
@@ -66,7 +66,7 @@ if (rootElement) {
 
   try {
     root.render(
-      <React.StrictMode>
+      // <React.StrictMode>
         <I18nextProvider i18n={i18n}>
           <LoadingProvider>
             <Router>
@@ -86,7 +86,7 @@ if (rootElement) {
             </Router>
           </LoadingProvider>
         </I18nextProvider>
-      </React.StrictMode>
+      // </React.StrictMode>
     );
     if (import.meta.env.VITE_DEBUG_MODE) {
       log('✅ React rendered successfully');
