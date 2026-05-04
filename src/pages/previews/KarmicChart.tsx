efc9b3bda71e5c8291a87bcf6bda3ae90f42451c
PirvuDanielCatalin <daniel.catalin.pirvu@gmail.com>
2026-05-04 22:34:48 +0300

    DevOps

--- src/pages/previews/KarmicChart.tsx
+++
@@ -1,5 +0,0 @@
-import KarmicChartPage from '@/pages/previews/karmic-chart/KarmicChartPage';
-
-// Main Component
-const KarmicChart = KarmicChartPage;
-export default KarmicChart;
--- src/pages/previews/NatalChart.tsx
+++
@@ -1,5 +0,0 @@
-import NatalChartPage from '@/pages/previews/natal-chart/NatalChartPage';
-
-// Main Component
-const NatalChart = NatalChartPage;
-export default NatalChart;
